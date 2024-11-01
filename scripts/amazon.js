1d8889a9c4f3353fc5134706cd1ed4e0065bab02
Diogo Teixeira <diogoantonio_77@hotmail.com>
2024-11-01 12:17:41 +0100

    Use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,30 +1,3 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090 //this is in cents because JavaScript has problems doing math with floats
-},{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-},{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}
-];
-
 let productsHTML = '';
 
 products.forEach((product,index) => {
@@ -81,3 +54,6 @@ products.forEach((product,index) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+
+//The benefits of Generating an HTML is that if we want to have new products, you dont need to be always writing HTML to every product. you just need to add the data to an array.
