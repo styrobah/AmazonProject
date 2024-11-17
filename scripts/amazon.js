ebb59f3a208c8d5b373bb81b636fbb51cdc21409
Diogo Teixeira <diogoantonio_77@hotmail.com>
2024-11-17 01:40:54 +0100

    Final result

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,87 +1,81 @@
-//this is a module. Modules = better way to organize our code 
-import { cart, addtoCart } from '../data/cart.js';
-import { products } from '../data/products.js';
-import { formatCurrency } from '../scripts/utils/money.js';
-//
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
 let productsHTML = '';
 
 products.forEach((product) => {
-    productsHTML += `
-        <div class="product-container">
-            <div class="product-image-container">
-                <img class="product-image"
-                src="${product.image}">
-            </div>
+  productsHTML += `
+    <div class="product-container">
+      <div class="product-image-container">
+        <img class="product-image"
+          src="${product.image}">
+      </div>
 
-            <div class="product-name limit-text-to-2-lines">
-                ${product.name}
-            </div>
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
 
-            <div class="product-rating-container">
-                <img class="product-rating-stars"
-                src="images/ratings/rating-${product.rating.stars * 10}.png">
-                <div class="product-rating-count link-primary">
-                ${product.rating.count}
-                </div>
-            </div>
-
-            <div class="product-price">
-                $${formatCurrency(product.priceCents)}
-            </div>
-
-            <div class="product-quantity-container">
-                <select>
-                <option selected value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                </select>
-            </div>
-
-            <div class="product-spacer"></div>
-
-            <div class="added-to-cart">
-                <img src="images/icons/checkmark.png">
-                Added
-            </div>
-
-            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
-                Add to Cart
-            </button>
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
         </div>
-    `;
-});
+      </div>
 
-function updateCartQuantity() {
-    let cartQuantity = 0;
-    cart.forEach((cartItem) =>{
-        cartQuantity += cartItem.quantity;
-    })
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-}
+      <div class="product-price">
+        $${formatCurrency(product.priceCents)}
+      </div>
+
+      <div class="product-quantity-container">
+        <select>
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+
+      <div class="product-spacer"></div>
+
+      <div class="added-to-cart">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
+
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.id}">
+        Add to Cart
+      </button>
+    </div>
+  `;
+});
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
-document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+
+function updateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
+}
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
     button.addEventListener('click', () => {
-        //to add the product to the cart it was needed to create a data attribute on the html to be accessible when clicking on a button
-        const productId = button.dataset.productId;
-        addtoCart(productId);
-        updateCartQuantity();
+      const productId = button.dataset.productId;
+      addToCart(productId);
+      updateCartQuantity();
     });
-});
-
-
-//The benefits of Generating an HTML is that if we want to have new products, you dont need to be always writing HTML to every product. you just need to add the data to an array.
-
-
-//The main idea of JavaScript is:
-// 1) Save the data
-// 2) Generate the HTML
-// 3) Make it interactive
+  });
--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,6 +1,5 @@
 export function formatCurrency(priceCents) {
-    return (priceCents/100).toFixed(2);
+  return (Math.round(priceCents) / 100).toFixed(2);
 }
 
-//if we use this export default it means when you are importing the format currency, you don't need to use the {}. See example on checkout.js. But you can only use this, if you only have one thing, in this case one function
 export default formatCurrency;
